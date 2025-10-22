835d3ba5b8d98dbb1e1c1dfee5eef01c63ccba79
SimonDEPRUN <160472926+SimonDEPRUN@users.noreply.github.com>
2025-10-22 16:57:50 +0200

    on commence la partie 2 avec les vue en ejs

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 import express from "express";
 import { connect } from "mongoose";
-import cors from "cors";
+// import cors from "cors";
+import methodeoverride from "method-override";
+
+
 import avionRoutes from "./routes/avionRoutes.js"
 import billetRoutes from "./routes/billetRoutes.js"
 import volRoutes from "./routes/volRoutes.js"
 import passagerRoutes from "./routes/passagerRoutes.js"
 
+
 const app = express();
+
+
 app.use(express.json());
+// app.use(cors());
+app.use(methodeoverride("_method"));
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/avions", avionRoutes);
-app.use("/billets", billetRoutes);
-app.use("/passagers", passagerRoutes);
-app.use("/vols", volRoutes);
 
-app.use(cors());
+
 connect("mongodb://127.0.0.1:27017/TP_ecf")
     .then(() => console.log("connecté à MongoDB"))
     .catch(err => console.error("Erreur MongoDB : ", err));
+
+
+app.set("view engine", "ejs");
+// app.set("views", "./views");
+
+app.use("/api/avion", avionRoutes);
+app.use("/api/billet", billetRoutes);
+app.use("/api/passager", passagerRoutes);
+app.use("/api/vol", volRoutes);
 
 app.get("/", (req, res) => res.send("API MongoDB en Node.js"));
 app.listen(3000, () => console.log("Serveur lancé sur http://localhost:3000"));
--- routes/avionRoutes.js
+++ routes/avionRoutes.js
@@ -4,44 +4,69 @@ const router = Router();
 
 router.post("/", async (req, res) => {
     try {
-        const avion = new Avion(req.body);
-        const savedAvion = await avion.save();
-        res.status(201).json(savedAvion);
+        const avion = await Avion.create(req.body);
+        res.redirect("/api/avion");
+        // const savedAvion = await avion.save();
+        // res.status(201).json(savedAvion);
     } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-})
-router.get("/", async (req, res) => {
-    const avions = await Avion.find();
-    res.json(avions);
-})
-router.get("/:id", async (req, res) => {
-    try {
-        const avion = await Avion.findById(req.params.id);
-        if (!avion) return res.status(404).json({ error: "Avion non trouvé" });
-        res.json(avion)
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-})
-router.put("/:id", async (req, res) => {
-    try {
-        const avion = await Avion.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!avion) return res.status(404).json({ error: "Avion non trouvé" });
-        res.json(avion)
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-})
-router.delete("/:id", async (req, res) => {
-    try {
 
-        const avion = await Avion.findByIdAndDelete(req.params.id);
-        if (!avion) return res.status(404).json({ error: "Avion non trouvé" });
-        res.json({ message: "avion Annulé" });
-    } catch (err) {
-        res.status(400).json({ error: err.message });
     }
+});
+router.put("/", async (req, res) => {
+    try {
+        const avion = await Avion.create(req.body);
+        res.redirect("/api/avion");
+    } catch (err) {
+
+    }
+});
+router.get("/", async (req, res) => {
+    try {
+        const avions = await Avion.find();
+        res.render("avion/index", { avions });
+        // res.json(avions);
+    } catch (err) {
+
+    }
+});
+router.get("/new", async (req, res) => {
+    try {
+        res.render("avion/new");
+    } catch (err) {
+
+    }
+});
+router.delete("/:id", async (req, res) => {
+    await Avion.findByIdAndDelete(req.params.id);
+    res.redirect("/api/avion");
 })
+// router.get("/:id", async (req, res) => {
+//     try {
+//         const avion = await Avion.findById(req.params.id);
+//         if (!avion) return res.status(404).json({ error: "Avion non trouvé" });
+//         res.json(avion)
+//     } catch (err) {
+//         res.status(400).json({ error: err.message });
+//     }
+// })
+// router.put("/:id", async (req, res) => {
+//     try {
+//         const avion = await Avion.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//         if (!avion) return res.status(404).json({ error: "Avion non trouvé" });
+//         res.json(avion)
+//     } catch (err) {
+//         res.status(400).json({ error: err.message });
+//     }
+// })
+// router.delete("/:id", async (req, res) => {
+//     try {
+
+//         const avion = await Avion.findByIdAndDelete(req.params.id);
+//         if (!avion) return res.status(404).json({ error: "Avion non trouvé" });
+//         res.json({ message: "avion Annulé" });
+//     } catch (err) {
+//         res.status(400).json({ error: err.message });
+//     }
+// })
 
 export default router;
